dee2154cd9581169cba960616492a54fe91f988f
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2023-08-15 23:37:14 +0500

    Update Program.cs

--- 100 days of challenge/1 day/2 day/Program.cs
+++ 100 days of challenge/1 day/2 day/Program.cs
@@ -14,14 +14,6 @@ using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'aVeryBigSum' function below.
-     *
-     * The function is expected to return a LONG_INTEGER.
-     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
-     */
-
     public static long aVeryBigSum(List<long> ar)
     {
         long sum = 0;
