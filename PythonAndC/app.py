a125890b98c75f97181edf22c43d990c482f33c4
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-04-30 00:03:12 +0500

    contexmanager and with

--- PythonAndC/app.py
+++ PythonAndC/app.py
@@ -1,39 +1,40 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
- 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
- 
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(150, 70, 93, 28))
- 
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(130, 149, 151, 31))
-        self.label.setText("")
- 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-        
-        # adding signal and slot
-        self.pushButton.clicked.connect(self.showmsg) 
- 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Click"))
-         
-    def showmsg(self):
-        # slot
-        self.label.setText("You clicked me")
- 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
- 
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
- 
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+# Traditional try-except block
+file = open("example.txt", "r")
+try:
+    # Perform some operations on the file
+    data = file.read()
+    print(data)
+except:
+    print("An error occurred while reading the file.")
+finally:
+    file.close()
+
+# Using a context manager
+with open("example.txt", "r") as file:
+    # Perform the same operations on the file
+    data = file.read()
+    print(data)
+    
+class OpenFile:
+    def __init__(self, filename, mode) :
+        self.filename = filename
+        self.mode = mode
+
+    def _enter__(self):
+        self. file = open(self. filename, self .mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        self. file. close()
+
+
+with OpenFile('sample.txt', 'W') as f:
+    f.write('Testing')
+    
+print(f.closed)
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+
