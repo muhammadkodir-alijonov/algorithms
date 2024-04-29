6582f03a12c344cba5f2a96c5ad0806b5540a5cf
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-04-30 00:13:04 +0500

    for r

--- PythonAndC/app.py
+++ PythonAndC/app.py
@@ -1,40 +1,14 @@
-# Traditional try-except block
-file = open("example.txt", "r")
-try:
-    # Perform some operations on the file
-    data = file.read()
-    print(data)
-except:
-    print("An error occurred while reading the file.")
-finally:
-    file.close()
+class FileWriter(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-# Using a context manager
-with open("example.txt", "r") as file:
-    # Perform the same operations on the file
-    data = file.read()
-    print(data)
-    
-class OpenFile:
-    def __init__(self, filename, mode) :
-        self.filename = filename
-        self.mode = mode
-
-    def _enter__(self):
-        self. file = open(self. filename, self .mode)
+    def __enter__(self):
+        self.file = open(self.file_name, "r")
         return self.file
 
-    def __exit__(self, exc_type, exc_val, traceback):
-        self. file. close()
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.file.close()
 
-
-with OpenFile('sample.txt', 'W') as f:
-    f.write('Testing')
-    
-print(f.closed)
-
-f = open("demofile2.txt", "a")
-f.write("Now the file has more content!")
-f.close()
-
+with FileWriter("test.txt") as file:
+    print(file.read())
 
