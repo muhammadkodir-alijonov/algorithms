3fcad567f2336646d578cc4be5a80776d98e1bbb
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-02 17:37:14 +0500

    three sum

--- ForLeetCode/leetcode.py
+++ ForLeetCode/leetcode.py
@@ -1,12 +1,26 @@
-# 1. three Sum
+#three sum
 nums = [-1, 0, 1, 2, -1, -4]
-def threesum(nums):
-    res = set()
-    nums.sort()
-    for i in range(len(nums)-2):
-        for j in range(i+1,len(nums)-1):
-            for k in range(j+1,len(nums)):
-                if nums[i]+nums[j]+nums[k] == 0:
-                    res.add((nums[i],nums[j],nums[k]))
-    return [list(triplet) for triplet in res]
-print(threesum(nums))    
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
+            
+                
