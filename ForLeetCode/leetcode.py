c3bda835885cfc120ac895d3d91daad63e6fdada
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-02 17:26:01 +0500

    threesum

--- ForLeetCode/leetcode.py
+++ ForLeetCode/leetcode.py
@@ -1,12 +1,12 @@
-#twu sum
-# 1. Two Sum
-nums = [2, 7, 11, 15]
-target = 9
-
-def twosum(nums,target):
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i]+nums[j]==target:
-                return [i,j]
-            return None
-print(twosum(nums,target))
+# 1. three Sum
+nums = [-1, 0, 1, 2, -1, -4]
+def threesum(nums):
+    res = set()
+    nums.sort()
+    for i in range(len(nums)-2):
+        for j in range(i+1,len(nums)-1):
+            for k in range(j+1,len(nums)):
+                if nums[i]+nums[j]+nums[k] == 0:
+                    res.add((nums[i],nums[j],nums[k]))
+    return [list(triplet) for triplet in res]
+print(threesum(nums))    
