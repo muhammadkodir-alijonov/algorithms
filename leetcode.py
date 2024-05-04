a9e5dfc951e85e37f5b42a48d9f291304a1e48ee
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-04 19:00:26 +0500

    leetcode 79 problem

--- leetcode.py
+++ leetcode.py
@@ -1,25 +1,22 @@
+#leetcode 79 problems
 class Solution(object):
-    def threeSumClosest(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        nums.sort()
-        closest = float('inf')
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == target:
-                    return total
-                if abs(total - target) < abs(closest - target):
-                    closest = total
-                if total < target:
-                    left += 1
-                else:
-                    right -= 1
-        return closest
-print(Solution().threeSumClosest([-1, 2, 1, -4], 1))  # 2
+    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]] 
+    word = "ABCCED"
+    def exist(self, board, word):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0] != board[i][j]:
+            return False
+        tmp = board[i][j]
+        board[i][j] = "#"
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
+s = Solution()
+print(s.exist(s.board, s.word))
