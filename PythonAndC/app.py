3267b0dff85aad21117d5c162e344ad0f67f5dcc
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-08 18:09:35 +0500

    leetcode 20

--- PythonAndC/app.py
+++
@@ -1,4 +0,0 @@
-
-#using with statament
-
-class MessageWriter(object):
--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,24 @@
-#leetcode 79 problems
+#leetcode 20
 class Solution(object):
-    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]] 
-    word = "ABCCED"
-    def exist(self, board, word):
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self.dfs(board, i, j, word):
-                    return True
-        return False
-    def dfs(self, board, i, j, word):
-        if len(word) == 0:
-            return True
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0] != board[i][j]:
-            return False
-        tmp = board[i][j]
-        board[i][j] = "#"
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
-        board[i][j] = tmp
-        return res
-s = Solution()
-print(s.exist(s.board, s.word))
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                if i == ')' and stack[-1] != '(':
+                    return False
+                if i == ']' and stack[-1] != '[':
+                    return False
+                if i == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+        return not stack
+    
+print(Solution().isValid("([])"))
