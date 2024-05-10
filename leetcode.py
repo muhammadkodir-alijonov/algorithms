1caf6558f35c911ce8040b8f79608377ad48e26f
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-11 00:16:27 +0500

    hashmap

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,26 @@
-class Node:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+from collections import Counter, defaultdict
 
-def reverse(head):
-    pre = None
-    curr = head
-    while curr is not None:
-        next_ = curr.next
-        pre = curr
-        curr = next_
-    return pre
-print(reverse(Node(1, Node(2, Node(3, Node(4))))).val)
 
+def build_counter(word):
+    counter = defaultdict(int)
+    for char in word:
+        counter[char] += 1
+    return counter
+
+
+def is_anagram(word1, word2) -> bool:
+    counter = build_counter(word1)
+    for char in word2:
+        counter[char] -= 1
+    for value in counter.values():
+        if value != 0:
+            return False
+    return True
+
+
+print(is_anagram("anagram", "nagaram"))  # True
+
+my_string = "codingiscool1"
+are_there_digits = [char.isdigit() for char in my_string]
+print(any(are_there_digits))
 
