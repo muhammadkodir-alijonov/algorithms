3e868ebb8dcbad63f799482c3872b2850a482df1
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-13 22:43:17 +0500

    stack

--- leetcode.py
+++ leetcode.py
@@ -1,26 +1,18 @@
-from collections import Counter, defaultdict
 
 
-def build_counter(word):
-    counter = defaultdict(int)
-    for char in word:
-        counter[char] += 1
-    return counter
-
-
-def is_anagram(word1, word2) -> bool:
-    counter = build_counter(word1)
-    for char in word2:
-        counter[char] -= 1
-    for value in counter.values():
-        if value != 0:
+def valid_brackets(s: str) -> bool:
+    stack = []
+    bracka = {")": "(", "]": "[", "}": "{"}
+    for char in s:
+        if char not in bracka:
+            stack.append(char)
+            continue
+        if not stack:
             return False
-    return True
+        last_opened = stack.pop()
+        if last_opened != bracka[char]:
+            return False
+    return not stack
 
 
-print(is_anagram("anagram", "nagaram"))  # True
-
-my_string = "codingiscool1"
-are_there_digits = [char.isdigit() for char in my_string]
-print(any(are_there_digits))
-
+print(valid_brackets("({[})"))
