c0f63c9c42b451daeee58fb989ce3d26fcd4809b
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-13 22:52:16 +0500

    stack 2

--- leetcode.py
+++ leetcode.py
@@ -6,13 +6,9 @@ def valid_brackets(s: str) -> bool:
     for char in s:
         if char not in bracka:
             stack.append(char)
-            continue
-        if not stack:
-            return False
-        last_opened = stack.pop()
-        if last_opened != bracka[char]:
+        elif not stack or bracka[char] != stack.pop():
             return False
     return not stack
 
 
-print(valid_brackets("({[})"))
+print(valid_brackets("(]"))
