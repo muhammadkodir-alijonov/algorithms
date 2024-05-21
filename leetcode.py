bf56368b27d5b280abe3e7318f9425f676326e23
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-21 12:12:09 +0500

    BST

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,24 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
-        self.next = next
+        self.left = left
+        self.right = right
+
 
 class Solution(object):
-    def oddEvenList(self, head):
-        if not head and not head.next:
-            return head
-        odd = head
-        even = head.next
-        even_head = even
-        while even and even.next:
-            odd.next = even.next
-            odd = odd.next
-            even.next = odd.next
-            even = even.next
-        odd.next = even_head
-        return head
-
-
-print("Test case 1")
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(4)
-head.next.next.next.next = ListNode(5)
-sol = Solution()
-result = sol.oddEvenList(head)
-while result:
-    print(result.val)
-    result = result.next
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        def dfs(node):
+            if not node:
+                return 0
+            if node.val < low:
+                return dfs(node.right)
+            if node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+        return dfs(root)
