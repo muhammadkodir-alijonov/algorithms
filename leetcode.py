67d3fd98fb2786e15438c808c1a7f72c0b31200d
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-21 12:04:32 +0500

    linkedlist

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,32 @@
-from collections import deque
-
-class TreeNode:
-    def __init__(self, val):
+class ListNode(object):
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.left = None
-        self.right = None
+        self.next = next
 
-def is_leaf(node):
-    return not node.left and not node.right
+class Solution(object):
+    def oddEvenList(self, head):
+        if not head and not head.next:
+            return head
+        odd = head
+        even = head.next
+        even_head = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
 
-def count_leaves(root: TreeNode) -> int:
-    total = 0
-    queue = deque([root])
-    while queue:
-        curr = queue.pop()
-        if not curr:
-            continue
-        if is_leaf(curr):
-            total += 1
-        queue.appendleft(curr.left)
-        queue.appendleft(curr.right)
-    return total
+
+print("Test case 1")
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+sol = Solution()
+result = sol.oddEvenList(head)
+while result:
+    print(result.val)
+    result = result.next
