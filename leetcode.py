d52ebe49b558c4f4c842481bbfd3abdf96a5d746
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-24 17:09:27 +0500

    stack for -+*/

--- leetcode.py
+++ leetcode.py
@@ -1,24 +1,19 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from typing import List
 
-
-class Solution(object):
-    def rangeSumBST(self, root, low, high):
-        """
-        :type root: TreeNode
-        :type low: int
-        :type high: int
-        :rtype: int
-        """
-        def dfs(node):
-            if not node:
-                return 0
-            if node.val < low:
-                return dfs(node.right)
-            if node.val > high:
-                return dfs(node.left)
-            return node.val + dfs(node.left) + dfs(node.right)
-        return dfs(root)
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in '+-*/':
+                b, a = stack.pop(), stack.pop()
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                else:
+                    stack.append(int(a / b))
+            else:
+                stack.append(int(token))
+        return stack[0]
