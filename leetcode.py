53d554d6dde4156fbd81723ce7e177bbe2a39a9e
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-05-25 23:05:07 +0500

    productExceptSelf

--- leetcode.py
+++ leetcode.py
@@ -1,8 +1,16 @@
 from typing import List
-
+#Input: nums = [1,2,3,4]
+#Output: [24,12,8,6]
 class Solution:
-    def kidsWithCandies(candies: List[int], extraCandies: int) -> List[bool]:
-        max_candies = max(candies)
-        return [candy + extraCandies >= max_candies for candy in candies]
-    
-print(Solution.kidsWithCandies([2,3,5,1,3],3))
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+#simple solution with O(n) time complexity and O(n) space complexity with prefic and suffix arra
+        n = len(nums)
+        pref = [1]*n
+        suff = [1]*n
+        for i in range(1,n):
+            pref[i] = pref[i-1]*nums[i-1]
+        for i in range(n-2,-1,-1):
+            suff[i] = suff[i+1]*nums[i+1]
+        for i in range(n):
+            nums[i] = pref[i]*suff[i]
+        return nums
