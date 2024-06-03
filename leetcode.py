f6b511686c74377b2b52f56afe9773fc9ade7331
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-03 22:36:17 +0500

    productExceptSelf

--- leetcode.py
+++ leetcode.py
@@ -1,3 +1,31 @@
+from typing import List
+
 class Solution:
-    def reverseWords(self, s: str) -> str:
-        return ' '.join(s.split()[::-1])
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        if n == 1:
+            return 1
+
+        i = 0
+        j = 0
+
+        while i < n:
+            count = 1
+            while i + 1 < n  and chars[i] == chars[i+1]:
+                count += 1
+                i += 1
+
+            chars[j] = chars[i]
+            j += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[j] = c
+                    j += 1
+
+            i += 1
+
+        return j
+    
+    
+print(Solution().compress(["a","a","b","b","c","c","c"])) # 6
