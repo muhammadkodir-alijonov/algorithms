a91bc5e0f9ba951453e6601b3b437ce76358a0bc
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-03 12:28:37 +0500

    reverseWords

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,3 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #solve with dic
-        dic = {}
-        for i in s:
-            if i in dic:
-                dic[i] += 1
-            else:
-                dic[i] = 1
-        for i in t:
-            if i in dic:
-                dic[i] -= 1
-                if dic[i] == 0:
-                    del dic[i]
-            else:
-                return False
-        return not dic
-
-print(Solution().isAnagram("anagram", "nagaram")) # True
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(s.split()[::-1])
