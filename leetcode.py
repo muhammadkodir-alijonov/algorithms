17b4a69a842482b33002090b504314acd6c0ebdd
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-07 20:02:41 +0500

    shaxmit oq va qora 2

--- leetcode.py
+++ leetcode.py
@@ -1,7 +1,19 @@
-x = int(input("x kordinatani kiriting: "))
-y = int(input("y kordinatani kiriting: "))
-z = x + y
-if x < 9 and y < 9 and z % 2 == 0:
-    print("Qora")
-else:
+x1 = int(input("x1 kordinatani kiriting: "))
+y2 = int(input("y1 kordinatani kiriting: "))
+x2 = int(input("x2 kordinatani kiriting: "))
+y1 = int(input("y2 kordinatani kiriting: "))
+z1 = x1 + y1
+z2 = x2 + y2
+
+if x1 < 9 and y1 < 9 and x2 < 9 and y2 < 9 and z1 % 2 !=0 and z2 % 2!=0:
     print("Oq")
+else:
+    print("Qora")
+if x1 < 9 and y1 < 9 and x2 < 9 and y2 < 9 and z1 % 2 ==0:
+    print("x1 va y1 Qora katakda")
+if x1 < 9 and y1 < 9 and x2 < 9 and y2 < 9 and z2 % 2 ==0:
+    print("x2 va y2 Qora katakda")
+if x1 < 9 and y1 < 9 and x2 < 9 and y2 < 9 and z1 % 2 !=0:
+    print("x1 va y1 Oq katakda")
+if x1 < 9 and y1 < 9 and x2 < 9 and y2 < 9 and z2 % 2 !=0:
+    print("x2 va y2 Oq katakda")
