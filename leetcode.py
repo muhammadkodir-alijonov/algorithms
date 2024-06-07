eb8bcd466aedf5fe52dae07257546452967e22f5
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-07 19:45:23 +0500

    shaxmit oq va qora

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,7 @@
-#Sonning raqamlari yigʼindisi va necha xonali ekanini aniqlash
-
-n = int(input("n sonni kiriting=>"))
-sum_ = 0
-count = 0
-while n > 0:
-    count += 1
-    qoldiq = n % 10
-    sum_ = qoldiq + sum_*10
-    n //=10
-    
-print(f"sonning teskarisi {sum_} va {count} xonali")
+x = int(input("x kordinatani kiriting: "))
+y = int(input("y kordinatani kiriting: "))
+z = x + y
+if x < 9 and y < 9 and z % 2 == 0:
+    print("Qora")
+else:
+    print("Oq")
