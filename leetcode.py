61fd684e6577b40d7cd36ff7107fae2157657439
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-07 12:26:40 +0500

    print(f"sonning teskarisi {sum_} va {count} xonali")

--- leetcode.py
+++ leetcode.py
@@ -1,28 +1,12 @@
-# 3 ta son berilgan shu sonlarning yig'indisi eng katta bo'ladigan iktasini chiqaruvchi programa tuzilsin c dastulash tilida 
+#Sonning raqamlari yigʼindisi va necha xonali ekanini aniqlash
 
-a = int(input("a sonning kiriting=>"))
-
-b = int(input("b sonning kiriting=>"))
-
-c = int(input("c sonning kiriting=>"))
-min_ = 0
-max_ = 0
-#eng kattasi uchun
-if a >= b and a >= c:
-    max_ = a
-    print(f"katta {max_}")
-elif b >= a and b >= c:
-    max_ = b
-    print(f"katta {max_}")
-else:
-    print(f"katta {c}")
+n = int(input("n sonni kiriting=>"))
+sum_ = 0
+count = 0
+while n > 0:
+    count += 1
+    qoldiq = n % 10
+    sum_ = qoldiq + sum_*10
+    n //=10
     
-#eng kichigi uchun
-if a <= b and a <= c:
-    min_ = a
-    print(f"kichik {min_}")
-elif b <= a and b <= c:
-    min_ = b
-    print(f"kichik {min_}")
-else:
-    print(f"kichik {min_}")
+print(f"sonning teskarisi {sum_} va {count} xonali")
