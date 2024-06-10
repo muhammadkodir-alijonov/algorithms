5a6c4a66afa41870680f6a122704e39ad89256b8
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-10 19:33:34 +0500

    maxSum

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,36 @@
-from typing import List
+# code
+import sys
 
-class Solution:
-    def maxOperations(nums: List[int], k: int) -> int:
-        nums.sort()
-        i, j = 0, len(nums) - 1
-        count = 0
-        while i < j:
-            if nums[i] + nums[j] == k:
-                count += 1
-                i += 1
-                j -= 1
-            elif nums[i] + nums[j] < k:
-                i += 1
-            else:
-                j -= 1
-        return count
+# O(n * k) solution for finding
+# maximum sum of a subarray of size k
+INT_MIN = -sys.maxsize - 1
 
-print(Solution.maxOperations([1,2,3,4,5], 5)) # 2
+# Returns maximum sum in a
+# subarray of size k.
+
+
+def maxSum(arr, n, k):
+
+    # Initialize result
+    max_sum = INT_MIN
+
+    # Consider all blocks
+    for i in range(n - k + 1):
+        current_sum = 0
+        for j in range(k):
+            current_sum = current_sum + arr[i + j]
+
+        # Update result if required.
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
+
+
+# Driver code
+arr = [1, 4, 2, 10, 2,
+       3, 1, 0, 20]
+k = 4
+n = len(arr)
+print(maxSum(arr, n, k))
+
+# This code is contributed by mits
