7d1734f0d66ccfdeedbd5c158d68c732d5003449
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-10 16:56:07 +0500

    maxOperations

--- leetcode.py
+++ leetcode.py
@@ -1,29 +1,19 @@
-import random
+from typing import List
 
-class RandomizedSet:
+class Solution:
+    def maxOperations(nums: List[int], k: int) -> int:
+        nums.sort()
+        i, j = 0, len(nums) - 1
+        count = 0
+        while i < j:
+            if nums[i] + nums[j] == k:
+                count += 1
+                i += 1
+                j -= 1
+            elif nums[i] + nums[j] < k:
+                i += 1
+            else:
+                j -= 1
+        return count
 
-    def __init__(self):
-        self.lst = []
-        self.map = {}
-
-    def insert(self, val: int) -> bool:
-        if val in self.map:
-            return False
-
-        self.lst.append(val)
-        self.map[val] = len(self.lst) - 1
-        return True
-
-    def remove(self, val: int) -> bool:
-        if not val in self.map:
-            return False
-
-        val_idx = self.map[val]
-        self.lst[val_idx] = self.lst[-1]
-        self.map[self.lst[-1]] = val_idx
-        self.lst.pop()
-        del self.map[val]
-        return True
-
-    def getRandom(self) -> int:
-        return random.choice(self.lst)
+print(Solution.maxOperations([1,2,3,4,5], 5)) # 2
