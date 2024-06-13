58023a6a92614b2bf3d7c91161d215e750fbbc86
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-13 18:35:16 +0500

    largestAltitude

--- leetcode.py
+++ leetcode.py
@@ -1,25 +1,13 @@
 from typing import List
 
-
-# Input nums = [0,1,1,1,0,1,1,0,1]
-# Output: 5
-
 class Solution:
-    def longestSubarray(nums: List[int]) -> int:
-        left = 0
-        right = 0
-        max_len = 0
-        num_zeros = 0
-        while right < len(nums):
-            if nums[right] == 0:
-                num_zeros += 1
+    def largestAltitude(self, gain: List[int]) -> int:
+        n = len(gain)
+        prefix_sum = [0]*(n+1)
+        for i in range(1, n+1):
+            prefix_sum[i] = prefix_sum[i-1] + gain[i-1]
+        max_sum = max(prefix_sum)
+        return max_sum
 
-            while num_zeros > 1:
-                if nums[left] == 0:
-                    num_zeros -= 1
-                left += 1
-            max_len = max(max_len, right - left)
-            right += 1
-        return max_len
-    
-print(Solution.longestSubarray([0,1,1,1,0,1,1,0,1]))
+
+print(Solution().largestAltitude([-5,1,5,0,-7]))    
