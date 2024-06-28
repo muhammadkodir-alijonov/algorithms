0c49acc3fc2a0f13affdf5842682f47fff2e7fdd
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-06-28 16:02:53 +0500

    6

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,22 @@
-secret_number = 9
-guess = -1 #taxmin
+# Boshlang'ich va oxirgi qiymatlarni belgilaymiz
+start = -10
+end = 13
 
+# Manfiy va musbat sonlarning yig'indisini saqlash uchun o'zgaruvchilar
+sum_negative = 0
+sum_positive = 0
 
+# Joriy qiymatni boshlang'ich qiymatga tenglaymiz
+current = start
 
-while guess != secret_number:
-    guess = int(input("Maxfiy raqamni toping: "))
-    if guess < secret_number:
-        print("Juda kichik!")
-    elif guess > secret_number:
-        print("Juda katta!")
-    else:
-        print("Tabriklaymiz! Siz to'g'ri topdingiz.")
+# While siklini boshlaymiz
+while current <= end:
+    if current < 0:
+        sum_negative += current
+    elif current > 0:
+        sum_positive += current
+    current += 1
+
+# Natijalarni chop etamiz
+print("Manfiy sonlar yig'indisi:", sum_negative)
+print("Musbat sonlar yig'indisi:", sum_positive)
