4b5f6d12c8d96bee5a2c174daa8b196cfe35ffee
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-04 22:54:31 +0500

    reverseList

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,17 @@
-from typing import List
-
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 class Solution:
-    def minDifference(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n <= 4:
-            return 0
-        nums.sort()
-        
-        case1 = nums[n-4] - nums[0]
-        case2 = nums[n-3] - nums[1]
-        case3 = nums[n-2] - nums[2]
-        case4 = nums[n-1] - nums[3]
-        
-        return min(case4,case3,case2,case1)
-    
-print(Solution().minDifference([1,5,0,10,14])) # 1
-        
+    def reverseList(self, head: ListNode) -> ListNode:
+        node = None
+
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+        return node
+
+print(Solution().reverseList([1,2,3,4,5]))
