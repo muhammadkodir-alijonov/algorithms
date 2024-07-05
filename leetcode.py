10bc40dbb44ea0c5a74c4e803b1bb9cb74f5ebbd
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-05 18:45:06 +0500

    commetn is nothing

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,20 @@
-# Definition for singly-linked list.
-class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
-class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        node = None
+son1 = float(input("Birinchi sonni kiriting: "))
+son2 = float(input("Ikkinchi sonni kiriting: "))
+son3 = float(input("Uchinchi sonni kiriting: "))
 
-        while head:
-            temp = head.next
-            head.next = node
-            node = head
-            head = temp
-        return node
+if son1 >= son2 and son1 >= son3:
+    eng_katta = son1
+elif son2 >= son1 and son2 >= son3:
+    eng_katta = son2
+else:
+    eng_katta = son3
 
-print(Solution().reverseList([1,2,3,4,5]))
+if son1 <= son2 and son1 <= son3:
+    eng_kichik = son1
+elif son2 <= son1 and son2 <= son3:
+    eng_kichik = son2
+else:
+    eng_kichik = son3
+
+print(f"Eng katta son: {eng_katta}")
+print(f"Eng kichik son: {eng_kichik}")
