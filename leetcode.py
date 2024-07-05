16121db129fe8ee2cb0bc64ee5b8500634783ea6
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-05 19:33:22 +0500

    longestCommonPrefix

--- leetcode.py
+++ leetcode.py
@@ -1,20 +1,11 @@
-son1 = float(input("Birinchi sonni kiriting: "))
-son2 = float(input("Ikkinchi sonni kiriting: "))
-son3 = float(input("Uchinchi sonni kiriting: "))
-
-if son1 >= son2 and son1 >= son3:
-    eng_katta = son1
-elif son2 >= son1 and son2 >= son3:
-    eng_katta = son2
-else:
-    eng_katta = son3
-
-if son1 <= son2 and son1 <= son3:
-    eng_kichik = son1
-elif son2 <= son1 and son2 <= son3:
-    eng_kichik = son2
-else:
-    eng_kichik = son3
-
-print(f"Eng katta son: {eng_katta}")
-print(f"Eng kichik son: {eng_kichik}")
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for s in strs[1:]:
+            while s.find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
