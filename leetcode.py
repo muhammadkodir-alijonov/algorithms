e88e660d095e9a4ee4f11e7b9eb0cfe40757cd86
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-07 20:13:58 +0500

    maximum69Number

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def maximum69Number (self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        for i in num:
-            if i == 6:
-                num = num.replace(i, 9, 1)
-                break
-        return num
+from typing import List
 
-print(Solution.maximum69Number(9969))
+def moveZeroes(nums: list) -> list:
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i +=1
+    return nums
+
+print(moveZeroes([0,1,0,3,12])) # [1,3,12,0,0]
