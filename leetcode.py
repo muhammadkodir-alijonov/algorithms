d8459a5eb18004e35cde971378b21b05c6db3031
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-07 22:31:16 +0500

    numWaterBottles

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,8 @@
-from typing import List
-
-def moveZeroes(nums: list) -> list:
-    i = 0
-    for j in range(len(nums)):
-        if nums[j] != 0:
-            nums[i], nums[j] = nums[j], nums[i]
-            i +=1
-    return nums
-
-print(moveZeroes([0,1,0,3,12])) # [1,3,12,0,0]
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        summ = numBottles
+        while numBottles >= numExchange:
+            new_b = numBottles//numExchange
+            summ += new_b
+            numBottles = new_b + numBottles%numExchange
+        return summ
