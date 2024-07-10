4a21eb7d1d62c3ec764f130e345807a8d122c011
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-10 23:33:03 +0500

    minOperations

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,12 @@
 from typing import List
 
 class Solution:
-    def minDifference(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n <= 4:
-            return 0
-        nums.sort()
-        
-        case1 = nums[n-4] - nums[0]
-        case2 = nums[n-3] - nums[1]
-        case3 = nums[n-2] - nums[2]
-        case4 = nums[n-1] - nums[3]
-        
-        return min(case4,case3,case2,case1)
-    
-print(Solution().minDifference([1,5,0,10,14])) # 1
-        
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for log in logs:
+            if log == '../':
+                if stack:
+                    stack.pop()
+            elif log != './':
+                stack.append(log)
+        return len(stack)
