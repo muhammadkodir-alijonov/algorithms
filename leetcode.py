922733cae2e7d9b73264afc6aa07e8cfdf3461a8
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-12 19:22:14 +0500

    twoSum

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,9 @@
-def is_prime(num):
-    if num < 2:
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target:
+                    return list(nums[i],nums[j])
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def print_primes_up_to(n):
-    print(f"{n} gacha bo'lgan tub sonlar:")
-    for i in range(2, n + 1):
-        if is_prime(i):
-            print(i, end=" ")
-
-# n ni kiriting
-n = int(input("n ni kiriting: "))
-print_primes_up_to(n)
