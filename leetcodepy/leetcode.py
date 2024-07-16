aad6e16d9cd5dfc2310bd5bf1acb09e85737b7dc
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-16 17:37:20 +0500

    maxProfit

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,10 +1,19 @@
+
+from typing import List
+#prices = [7,1,5,3,6,4]
+#output = 5
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        hash_tabel = {}
-        start = max_length = 0
-        for i in range(len(s)):
-            if s[i] in hash_tabel:
-                start = max(start,hash_tabel[s[i]+1])
-            hash_tabel[s[i]] = 1
-            max_length = max(max_length,i-start+1)
-        return max_length
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in prices:
+            if min_price > i:
+                min_price = i
+            elif i - min_price > max_profit:
+                max_profit = i - min_price
+        return max_profit
+
+prices = [7,1,5,3,6,4]
+sol = Solution()
+sol.maxProfit(prices)
+print(sol.maxProfit(prices))
