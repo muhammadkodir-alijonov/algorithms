62c0244371f7d3c31cf3cbee15e6a66de952fe3d
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-16 17:36:39 +0500

    maxProfit

--- LearningAlgorithms/Sliding Window/slidingwindow.py
+++ LearningAlgorithms/Sliding Window/slidingwindow.py
@@ -1,12 +1,18 @@
-#sliding window algorithms
-def slidingwindow(arr, k):
-    n = len(arr)
-    if n < k:
-        return []
-    result = []
-    window_sum = sum(arr[:k])
-    result.append(window_sum)
-    for i in range(n-k):
-        window_sum = window_sum - arr[i] + arr[i+k]
-        result.append(window_sum)
-    return result
+from typing import List
+#prices = [7,1,5,3,6,4]
+#output = 5
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in prices:
+            if min_price > i:
+                min_price = i
+            elif i - min_price > max_profit:
+                max_profit = i - min_price
+        return max_profit
+    
+prices = [7,1,5,3,6,4]
+sol = Solution()
+sol.maxProfit(prices)
+print(sol.maxProfit(prices))
