b8edd602704a7cf65286b2a66f60a29aff0cb72f
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-18 15:34:06 +0500

    isHappy

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,19 +1,15 @@
+#Input: n = 19 True
 
-from typing import List
-#prices = [7,1,5,3,6,4]
-#output = 5
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for i in prices:
-            if min_price > i:
-                min_price = i
-            elif i - min_price > max_profit:
-                max_profit = i - min_price
-        return max_profit
-
-prices = [7,1,5,3,6,4]
-sol = Solution()
-sol.maxProfit(prices)
-print(sol.maxProfit(prices))
+    def isHappy(self, n: int) -> bool:
+        korilgan_sonlar = []
+        while True:
+            digit = n//10
+            korilgan_sonlar.append(digit)
+            summ += digit**2
+            if summ == 1:
+                return True
+            else:
+                continue
+        return False
+            
