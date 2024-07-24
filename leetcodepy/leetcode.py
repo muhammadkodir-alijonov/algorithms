f6b2d09b2e46123d9b8d1a2e7d2911af855e2106
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-24 17:27:48 +0500

    removeElement

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,5 +1,12 @@
 from typing import List
 
 class Solution:
-    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        return list({min(row) for row in matrix} & {max(col) for col in zip(*matrix)})
+    def removeElement(self, nums: List[int], val: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[res] = nums[i]
+                res+=1
+        return res
+    
+print(Solution().removeElement([0,1,2,2,3,0,4,2], 2))   
