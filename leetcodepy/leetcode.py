16659595c73639d3efa880274f771a612d8b2f79
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-28 18:37:43 +0500

    minimumTotal

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,12 +1,11 @@
 from typing import List
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        j = 0
-        for i in range(len(nums)):
-            if nums[i] != nums[j]:
-                j +=1    
-                nums[j] = nums[i]
-        return j+1
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+        return triangle[0][0]
+
+print(Solution().minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]]) == 11)
