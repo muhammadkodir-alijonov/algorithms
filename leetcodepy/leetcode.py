1606c6f8520a0cfaa6694938e075abf6df1fbea6
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-01 18:30:25 +0500

    findPeaks

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,12 +1,9 @@
 from typing import List
-# n = 5
-#[-2,-1,0,1,2].
+
 class Solution:
-    def sumZero(self, n: int) -> List[int]:
-        sum_zero = []
-        if n%2 != 0:
-            sum_zero.append(0)
-        for i in range(1,n//2+1):
-            sum_zero.append(i)
-            sum_zero.append(-i)
-        return sum_zero
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        peaks = []
+        for i in range(1, len(mountain) - 1):
+            if mountain[i] > mountain[i - 1] and mountain[i] > mountain[i + 1]:
+                peaks.append(i)
+        return peaks
