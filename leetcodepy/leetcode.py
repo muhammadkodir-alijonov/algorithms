c8bc0d2fa9a3fd800d34c84f11c3dcf93260ca71
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-01 13:42:56 +0500

    lengthOfLongestSubstring

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,7 +1,12 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        summ = 0
-        n = len(nums)
-        for i in range(len(nums)):
-            summ += nums[i]
-        return n*(n+1)/2 -summ
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_dict = {}
+        start = max_length = 0
+        for i in range(len(s)):
+            if s[i] in char_dict:
+                start = max(start, char_dict[s[i]] + 1)
+            char_dict[s[i]] = i
+            max_length = max(max_length, i - start + 1)
+        return max_length
+    
+print(Solution().lengthOfLongestSubstring("acabcbb"))  # 3
