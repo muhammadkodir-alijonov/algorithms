da5a5e404cef77e0d90e648ce80e4a40f35aa3b8
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-03 15:08:32 +0500

    busyStudent

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,3 +1,9 @@
+from typing import List
+
 class Solution:
-    def addDigits(self, num: int) -> int:
-        return 1 + (num - 1) % 9 if num > 0 else 0
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                count += 1
+        return count
