996744caa08683284c3151b97ed0cd4d930abd25
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-03 15:27:13 +0500

    islandPerimeter

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,9 +1,16 @@
 from typing import List
 
 class Solution:
-    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
-        count = 0
-        for i in range(len(startTime)):
-            if startTime[i] <= queryTime <= endTime[i]:
-                count += 1
-        return count
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        perimetr = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimetr += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimetr -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimetr -= 2
+        return perimetr
