62a4b64526536345a320b7dedf0d5540fa60e1b1
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-07 00:52:41 +0500

    countBits

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,15 +1,5 @@
 from typing import List
 
 class Solution:
-    def canAliceWin(self, nums: List[int]) -> bool:
-        summ1 = 0
-        summ = 0
-        for num in nums:
-            if num<=9:
-               summ += num
-            else:
-                summ1 += num
-        if summ == summ1:
-            return False
-        else:
-            return True
+    def countBits(self, n: int) -> List[int]:
+        return [bin(i).count('1') for i in range(n + 1)]
