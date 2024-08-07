608170bb2b643ac9e6e1aa40d2ef43bd535e98d2
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-07 10:15:00 +0500

    sortColors bubble

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -5,10 +5,12 @@ class Solution:
         """
         #nums = [2,0,2,1,1,0]
         #[0,0,1,1,2,2]
-        for i in range(len(nums)-1):
-            min_index = i
-            for j in range(i+1,len(nums)):
-                if nums[min_index]>nums[j]:
-                    min_index = j
-            nums[i],nums[min_index] = nums[min_index],nums[i]
-        
+        n = len(nums)
+        for i in range(n):
+            swapped = False
+            for j in range(0,n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+            if not swapped:
+                break
