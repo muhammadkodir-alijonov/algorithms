882e10b7b1fab4d066aad4bef7076307ea0be28b
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-07 09:54:22 +0500

    sortColors insertion

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,5 +1,14 @@
-from typing import List
-
 class Solution:
-    def countBits(self, n: int) -> List[int]:
-        return [bin(i).count('1') for i in range(n + 1)]
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        #nums = [2,0,2,1,1,0]
+        #[0,0,1,1,2,2]
+        for i in range(len(nums)-1):
+            min_index = i
+            for j in range(i+1,len(nums)):
+                if nums[min_index]>nums[j]:
+                    min_index = j
+            nums[i],nums[min_index] = nums[min_index],nums[i]
+        
