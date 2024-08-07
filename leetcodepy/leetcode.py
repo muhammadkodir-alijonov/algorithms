6397119e52e45280d52bf0b5cfaf049d09f68320
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-07 10:43:47 +0500

    sortColors merge

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -5,12 +5,12 @@ class Solution:
         """
         #nums = [2,0,2,1,1,0]
         #[0,0,1,1,2,2]
-        n = len(nums)
-        for i in range(n):
-            swapped = False
-            for j in range(0,n-i-1):
-                if nums[j]>nums[j+1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-                    swapped = True
-            if not swapped:
-                break
+        #merge sort
+        def merge_sort(nums, left, right):
+            if left >= right:
+                return
+            mid = (left + right) // 2
+            merge_sort(nums, left, mid)
+            merge_sort(nums, mid + 1, right)
+            merge(nums, left, mid, right)
+            
