443e8a6074e13e23b1da850b34c58ea03a6e7776
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-16 23:31:42 +0500

    combinationSum

--- leetcodepy/leetcode-mukhammadkodir.py
+++ leetcodepy/leetcode-mukhammadkodir.py
@@ -1,7 +1,19 @@
+from typing import List
+
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        it = iter(t)
-        for char in s:
-            if char in it:
-                return all(char)
-        return False
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res , sol = [], []
+        nums = candidates
+        n = len(candidates)
+        def backtrack(i,cur_sum):
+            if cur_sum == target:
+                res.append(sol[:])
+                return 
+            if cur_sum > target or i == n:
+                return 
+            backtrack(i+1,cur_sum)
+            sol.append(nums[i])
+            backtrack(i,cur_sum+nums[i])
+            sol.pop()
+        backtrack(0,0)
+        return res
