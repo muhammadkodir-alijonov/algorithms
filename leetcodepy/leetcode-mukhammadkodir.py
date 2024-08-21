7ca120d9e4545a07424e911819b28cc67d225fc2
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-21 23:29:53 +0500

    bfs

--- leetcodepy/leetcode-mukhammadkodir.py
+++ leetcodepy/leetcode-mukhammadkodir.py
@@ -1,17 +1,18 @@
-from typing import Deque
+from collections import deque
 
 class Treenode:
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
+
 def is_leaf(node):
     return not node.left and not node.right
 
-def count_leaves(root:Treenode) -> int:
+def count_leaves(root: Treenode) -> int:
     total = 0
-    queue = Deque[root]
-    
+    queue = deque([root])
+
     while queue:
         curr = queue.pop()
         if not curr:
@@ -21,3 +22,12 @@ def count_leaves(root:Treenode) -> int:
         queue.appendleft(curr.left)
         queue.appendleft(curr.right)
     return total
+
+# Example usage:
+root = Treenode(3)
+root.left = Treenode(9)
+root.right = Treenode(20)
+root.right.left = Treenode(15)
+root.right.right = Treenode(7)
+
+print(count_leaves(root)) # 3
