6afa5a0929b51e4afa0ea71c593205c70b14a771
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-08-31 15:52:13 +0500

    summaryRanges

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,4 +1,19 @@
 from typing import List
-
+#nums = [0,1,2,4,5,7]
+#Output: ["0->2","4->5","7"]
 class Solution:
-    def findClosestNumber(self, nums: List[int]) -> int:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        i = 0
+        ans = []
+        
+        while i < len(nums):
+            start = nums[i]
+            
+            while i < len(nums) - 1 and nums[i] + 1 == nums[i+1]:
+                i+=1
+            if start != nums[i]:
+                ans.append(str(start) + '->' + str(nums[i]))
+            else:
+                ans.append(str(nums[i]))
+            i+=1
+        return ans
