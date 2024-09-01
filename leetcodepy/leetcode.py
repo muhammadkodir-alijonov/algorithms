c50d3d34cc6a1b66a718fdd6445cf9d6043af9d4
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-01 15:20:12 +0500

    rotate

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,34 +1,14 @@
 from typing import List
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:
-            return []
-
-        num_rows = len(matrix)
-        num_columns = len(matrix[0])
-        res = []
-
-        left, right = 0, num_columns - 1
-        top, bottom = 0, num_rows - 1
-
-        while left <= right and top <= bottom:
-            for j in range(left, right + 1):
-                res.append(matrix[top][j])
-            top += 1
-
-            for i in range(top, bottom + 1):
-                res.append(matrix[i][right])
-            right -= 1
-
-            if top <= bottom:
-                for j in range(right, left - 1, -1):
-                    res.append(matrix[bottom][j])
-                bottom -= 1
-
-            if left <= right:
-                for i in range(bottom, top - 1, -1):
-                    res.append(matrix[i][left])
-                left += 1
-
-        return res
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+        return matrix
