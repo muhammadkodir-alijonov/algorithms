52aced9d6ff865dffca600cc622515c3e687db43
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-03 21:58:03 +0500

    canConstruct

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,14 +1,20 @@
-from typing import List
+from collections import Counter
+
 
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(n):
-            matrix[i].reverse()
-        return matrix
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = {}
+        for i in magazine:
+            if i in counter:
+                counter[i] +=1
+            else:
+                counter[i] = 1
+                
+        for i in ransomNote:
+            if i not in counter:
+                return False
+            elif counter[i] == 1:
+                del counter[i]
+            else:
+                counter[i] -= 1
+        return True
