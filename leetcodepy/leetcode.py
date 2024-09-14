c15f61b3368f88c3964f6f6206c751e033f83db5
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-14 11:31:00 +0500

    permute

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,22 +1,8 @@
+import itertools
 from typing import List
 
 class Solution:
-    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        counter = {}
-
-        for letter in allowed:
-            if letter not in counter:
-                counter[letter] = 0
-            counter[letter] += 1
-
-        res = 0
-        for word in words:
-            is_consistent = True
-            for char in word:
-                if char not in counter:
-                    is_consistent = False
-                    break
-            if is_consistent:
-                res += 1
-
-        return res
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = list(itertools.permutations(nums))
+        return [list(x) for x in res]
+        
