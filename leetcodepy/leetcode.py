b0158018964754ddb0dc754b5cfaefbea4542f08
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-16 00:32:44 +0500

    sortedSquares

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,8 +1,6 @@
+from typing import List
+from collections import Counter
+
 class Solution:
-    def breakPalindrome(self, palindrome: str) -> str:
-        if len(palindrome) == 1:
-            return ""
-        for i in range(len(palindrome) // 2):
-            if palindrome[i] != 'a':
-                return palindrome[:i] + 'a' + palindrome[i + 1:]
-        return palindrome[:-1] + 'b'
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x**2 for x in nums])
