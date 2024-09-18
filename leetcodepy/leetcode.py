86da2862ee11ba3e8ebd29a0b625899d79a5f99e
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-18 15:05:38 +0500

    threeSum

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,14 +1,22 @@
-# Definition for a binary tree node.
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root:
-            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-            return root
-        return None
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1
+                elif s > 0:
+                    r +=1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
