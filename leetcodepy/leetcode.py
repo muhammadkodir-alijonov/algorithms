70091baf157b2aec664f4114e2d255641d999ef1
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-19 21:54:35 +0500

    MinStack

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,14 +1,28 @@
-from typing import List
+class MinStack:
 
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        max_area = 0
-        left = 0
-        right = len(height) - 1
-        while left < right:
-            max_area = max(max_area, min(height[left], height[right]) * (right - left))
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-        return max_area
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
