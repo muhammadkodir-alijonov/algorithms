2e20d2971eab4439ba09a84be03391f39d7bb3ba
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-27 11:26:01 +0500

    groupAnagrams

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,12 +1,12 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        res = [0] * len(temperatures)
-        for i, t in enumerate(temperatures):
-            while stack and t > temperatures[stack[-1]]:
-                cur = stack.pop()
-                res[cur] = i - cur
-            stack.append(i)
-        return res
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            key = tuple(sorted(s))
+            anagrams[key].append(s)
+
+        return list(anagrams.values())
