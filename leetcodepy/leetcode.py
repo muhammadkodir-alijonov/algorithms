7fa9fe65fff51b779e82f61f13533a37628136d9
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-28 15:15:53 +0500

    removeNthFromEnd

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -5,30 +5,27 @@ class ListNode:
         self.val = x
         self.next = next
 
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        mid = slow.next
-        slow.next = None
-        left = self.sortList(head)
-        right = self.sortList(mid)    
-        return self.mergeTwoLists(left,right)
-    
-    def mergeTwoLists(self,l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        current = dummy
-        while l1 and l2:
-            if l1.val <= l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
-            current = current.next
-        current.next = l1 if l1 else l2
-        return dummy.next
+def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+    # Dummy node to simplify edge cases (e.g., removing the first node)
+    dummy = ListNode(0)
+    dummy.next = head
+
+    fast = dummy
+    slow = dummy
+
+    # 1. Fast pointerni oldinga n qadam yuritamiz
+    for _ in range(n):
+        fast = fast.next
+
+    # 2. Ikkala pointerni oxirigacha bir vaqtda yuritamiz
+    while fast.next:
+        fast = fast.next
+        slow = slow.next
+
+    # 3. Sekin pointerni olib tashlash kerak bo'lgan elementga yetkazamiz
+    slow.next = slow.next.next
+
+    # 4. Ro'yxatning yangi boshini qaytarish
+    return dummy.next
+
+            
