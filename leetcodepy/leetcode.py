3bf323f8bc52a5ba66ae764f165af9ba9384646e
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-09-28 14:43:33 +0500

    sortList

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -6,15 +6,29 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        return self.recursion(list1, list2)
-
-    def recursion(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        if list1.val > list2.val:
-            return ListNode(list2.val, self.recursion(list2.next, list1))
-        else:
-            return ListNode(list1.val, self.recursion(list1.next, list2))
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(mid)    
+        return self.mergeTwoLists(left,right)
+    
+    def mergeTwoLists(self,l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next = l1 if l1 else l2
+        return dummy.next
