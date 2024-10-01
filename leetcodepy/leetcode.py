416055aa188c25aaacb135b1b57d7ac773cb2561
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-01 18:08:27 +0500

    groupAnagrams

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,21 +1,12 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        row, col = len(matrix), len(matrix[0])
-        # using set is removing duplication zero 
-        rows_to_zero = set()
-        cols_to_zero = set()
-
-        # First pass to find all rows and columns that need to be zeroed
-        for i in range(row):
-            for j in range(col):
-                if matrix[i][j] == 0:
-                    rows_to_zero.add(i)
-                    cols_to_zero.add(j)
-
-        # Second pass to set the rows and columns to zero
-        for i in range(row):
-            for j in range(col):
-                if i in rows_to_zero or j in cols_to_zero:
-                    matrix[i][j] = 0
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord("a")] +=1
+            res[tuple(count)].append(s)
+        return res.values()
