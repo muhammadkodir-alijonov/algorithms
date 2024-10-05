5be56913feb34ade125096f54e8f1e5bacf4c635
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-05 20:47:14 +0500

    rotate

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,15 +1,6 @@
 from typing import List
 
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n
-        self.reverse(nums, 0, n - 1)
-        self.reverse(nums, 0, k - 1)
-        self.reverse(nums, k, n - 1)
-
-    def reverse(self, nums: List[int], l: int, r: int):
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums) // 2]
