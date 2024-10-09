423a410bf17003144748d0c9dbe05ca739d2fe00
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-09 16:08:34 +0500

    minAddToMakeValid

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,12 +1,15 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        hash_map = {}
-        start = max_length = 0
-        for i in range(len(s)):
-            if s[i] in hash_map:
-                start = max(start,hash_map[s[i]]+1)
-            hash_map[s[i]] = i
-            max_length = max(max_length,i-start+1)
-        return max_length
+    def minAddToMakeValid(self, s: str) -> int:
+        ochiq = 0  # Yopilmagan ochiq qavslar soni
+        qoshimcha = 0  # Qo'shish kerak bo'lgan qavslar soni
+        for qavs in s:
+            if qavs == '(':
+                ochiq += 1  # Yopilishini kutayotgan ochiq qavslar soni oshadi
+            else:
+                if ochiq > 0:
+                    ochiq -= 1  # Yopilayotgan ochiq qavs
+                else:
+                    qoshimcha += 1  # Noto'g'ri yopiq qavs bo'lsa, ochiq qavs qo'shish kerak bo'ladi
+        return qoshimcha + ochiq  # Yopilmagan ochiq qavslar sonini qo'shamiz
 
-print(Solution().lengthOfLongestSubstring("abcabcbb")) # 3
+print(Solution().minAddToMakeValid("((())"))
