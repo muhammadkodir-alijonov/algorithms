d16dd9e08508e232dea7d5000de1473f651dfe8b
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-09 15:16:22 +0500

    lengthOfLongestSubstring

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,6 +1,12 @@
-from typing import List
-
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hash_map = {}
+        start = max_length = 0
+        for i in range(len(s)):
+            if s[i] in hash_map:
+                start = max(start,hash_map[s[i]]+1)
+            hash_map[s[i]] = i
+            max_length = max(max_length,i-start+1)
+        return max_length
+
+print(Solution().lengthOfLongestSubstring("abcabcbb")) # 3
