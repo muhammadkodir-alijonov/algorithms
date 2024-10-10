14af7a58998e9369867ac83d56a5c7bf2cf72a6e
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-10 12:07:35 +0500

    Todo

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,16 +1,58 @@
-class Solution:
-    def minAddToMakeValid(self, s: str) -> int:
-        stack = []  # Stack ochiq qavslar uchun
-        count = 0  # Qo'shimcha qavslar soni
+class Todo:
+    def __init__(self):
+        # Bo'sh todo list yaratamiz
+        self.tasks = []
 
-        for qavs in s:
-            if qavs == '(':
-                stack.append(qavs)  # Ochiq qavsni stackga qo'shamiz
-            else:  # Agar yopiq qavs bo'lsa
-                if stack:
-                    stack.pop()  # Ochiq qavsni yopamiz (stackdan olib tashlaymiz)
-                else:
-                    count += 1  # Noto'g'ri yopiq qavs, ochiq qavs kerak bo'lad
-        return count + len(stack)
-    
-print(Solution().minAddToMakeValid("())"))  # 1
+    def add_task(self, task_name):
+        # Vazifani ro'yxatga qo'shamiz
+        self.tasks.append(task_name)
+        print(f"Vazifa qo'shildi: {task_name}")
+
+    def remove_task(self, task_index):
+        # Vazifani indeks bo'yicha o'chiramiz
+        if 0 <= task_index < len(self.tasks):
+            removed_task = self.tasks.pop(task_index)
+            print(f"Vazifa o'chirildi: {removed_task}")
+        else:
+            print("Bunday vazifa yo'q")
+
+    def update_task(self, task_index, new_task_name):
+        # Indeks bo'yicha vazifani yangilash
+        if 0 <= task_index < len(self.tasks):
+            old_task = self.tasks[task_index]
+            self.tasks[task_index] = new_task_name
+            print(f"Vazifa yangilandi: '{old_task}' -> '{new_task_name}'")
+        else:
+            print("Bunday vazifa yo'q")
+
+    def show_tasks(self):
+        # Barcha vazifalarni ko'rsatish
+        if self.tasks:
+            print("Todo ro'yxati:")
+            for i, task in enumerate(self.tasks, start=1):
+                print(f"{i}. {task}")
+        else:
+            print("Todo ro'yxat bo'sh")
+
+# Todo obyektini yaratish
+my_todo = Todo()
+
+# Vazifalar qo'shish
+my_todo.add_task("Python darsini o'rganish")
+my_todo.add_task("Uy vazifasini bajarish")
+my_todo.add_task("Kitob o'qish")
+
+# Barcha vazifalarni ko'rsatish
+my_todo.show_tasks()
+
+# 2-vazifani yangilash
+my_todo.update_task(1, "Matematika vazifasini bajarish")
+
+# Vazifalarni qayta ko'rsatish
+my_todo.show_tasks()
+
+# 1-vazifani o'chirish
+my_todo.remove_task(0)
+
+# Qolgan vazifalarni ko'rsatish
+my_todo.show_tasks()
