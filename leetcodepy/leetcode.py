aa4c3427de702112d149978a3668530c3c1f0624
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-16 00:36:50 +0500

    rob

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -2,13 +2,17 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
+        if len(nums) == 0:
             return 0
         if len(nums) == 1:
             return nums[0]
-        if len(nums) == 2:
-            return max(nums)
-        return max(self.rob(nums[1:]), self.rob(nums[:-1]))
 
-print(Solution().rob([2,7,9,2,1])) 
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
+print(Solution().rob([2, 7, 9, 3, 1]))  # 12
