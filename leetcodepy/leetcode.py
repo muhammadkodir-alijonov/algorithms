2f28b04df5c9d3e32500e8d0a7a6c246c45e00b5
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-16 00:44:01 +0500

    rob

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,18 +1,11 @@
 from typing import List
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-
-        return dp[-1]
-print(Solution().rob([2, 7, 9, 3, 1]))  # 12
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i in range(len(nums)):
+            if i > farthest:
+                return False
+            farthest = max(farthest, i + nums[i])
+    
+print(Solution().canJump([2,3,0,1,4])) # True
