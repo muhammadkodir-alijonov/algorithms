5d78b3e82a9c7331ab0ab594fde8dc1d70701bb6
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-16 00:51:26 +0500

    rob

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,11 +1,19 @@
 from typing import List
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        farthest = 0
-        for i in range(len(nums)):
-            if i > farthest:
-                return False
-            farthest = max(farthest, i + nums[i])
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        dp = [0] * n
+        for i in range(n):
+            if i + nums[i] >= n - 1:
+                return dp[i] + 1
+            for j in range(i + 1, i + nums[i] + 1):
+                if dp[j] == 0:
+                    dp[j] = dp[i] + 1
+                else:
+                    dp[j] = min(dp[j], dp[i] + 1)
+        return dp[n - 1]
     
-print(Solution().canJump([2,3,0,1,4])) # True
+print(Solution().jump([2,3,1,1,4]))  # 2
