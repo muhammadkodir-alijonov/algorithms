61ae7dd030b8742ce4d83a2bba85784e63095fd8
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-16 20:36:45 +0500

    checkPerfectNumber

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,23 +1,21 @@
-from typing import List
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 1:
+            return False
+        count = 1
+        sqrt_num = int(num**0.5)
+        for i in range(2, sqrt_num+1):
+            if num%i == 0:
+                count+=i
+                if i != num//i:
+                    count+=num//i
+        return count == num
 
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return 0
 
-        jumps = 0
-        farthest = 0
-        currentJumpEnd = 0
-
-        for i in range(n - 1):
-            # Eng uzoq erishish mumkin bo'lgan indeksni yangilab boramiz
-            farthest = max(farthest, i + nums[i])
-
-            # Agar hozirgi sakrash oxiriga yetgan bo'lsa
-            if i == currentJumpEnd:
-                jumps += 1
-                currentJumpEnd = farthest
-
-        return jumps
-print(Solution().jump([2,3,1,1,4])) # 2
+num = 28
+solution = Solution()
+print(solution.checkPerfectNumber(num))
