3833e56f20ccaefd41ece7940aed7358a654c87e
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-17 14:28:01 +0500

    maximumSwap

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,14 +1,17 @@
-class Solution(object):
-    def isUgly(self, n):
-        if n <= 0:
-            return False
-        while (n % 2 == 0):
-            n /= 2
-        while (n % 3 == 0):
-            n /= 3
-        while (n % 5 == 0):
-            n /= 5
-            
-        return n == 1
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        str_num = list(str(num))
+        n = len(str_num)
+        max_idx = n - 1
+        x, y = -1, -1
 
-print(Solution().isUgly(64))  # True
+        for i in range(n - 1, -1, -1):
+            if str_num[i] > str_num[max_idx]:
+                max_idx = i
+            elif str_num[i] < str_num[max_idx]:
+                x, y = i, max_idx
+
+        if x != -1:
+            str_num[x], str_num[y] = str_num[y], str_num[x]
+
+        return int(''.join(str_num))
