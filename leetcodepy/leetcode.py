ae26ad8188d2f65268709548649794b26b10ced8
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-19 22:34:11 +0500

    singleNumber

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,15 +1,11 @@
+from typing import List
+
 class Solution:
-    def checkPerfectNumber(self, num: int) -> bool:
-        if num<=1:
-            return False
-        count = 1
-        my_sqrt = int(num**0.5)
-        
-        for i in range(2,my_sqrt+1):
-            if num%i == 0:
-                count += i
-                if i != num//i:
-                    count += num//i
-        return count == num
-    
-print(Solution().checkPerfectNumber(28)) # True
+    def singleNumber(self, nums: List[int]) -> int:
+        list = []
+        for i in nums:
+            if i in list:
+                list.remove(i)
+            else:
+                list.append(i)
+        return list[0]
