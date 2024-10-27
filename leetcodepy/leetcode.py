11ff87fc4f809355910c3ecfbc1fe9e665e75688
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-10-27 23:32:30 +0500

    canJump

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,15 +1,12 @@
 from typing import List
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-        n = len(nums)
-        while i < n:
-            if nums[i] == val:
-                nums[i] = nums[n-1]
-                n-=1
-            else:
-                i+=1
-        return n
+    def canJump(self, nums: List[int]) -> bool:
+        farthest = 0
+        for i in range(len(nums)):
+            if i > farthest:
+                return False
+            farthest = max(farthest, i + nums[i])
+        return True
 
-print(Solution().removeElement([3,2,2,3], 3)) # 2s
+print(Solution().canJump([2,1,1,0,4])) # True
