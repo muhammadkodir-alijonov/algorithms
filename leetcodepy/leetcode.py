9023afafac54c6591c2478fe0153c4e987fba77b
Мухаммадкодир Алижонов <muxammadqodir.a@fido.uz>
2024-10-31 06:04:56 -0700

    smth

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,11 +1,5 @@
 from typing import List
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        list = []
-        for i in nums:
-            if i in list:
-                list.remove(i)
-            else:
-                list.append(i)
-        return list[0]
+    def hIndex(self, citations: List[int]) -> int:
+        pass
