4c014b43370d799ddc7f502bd40be10b04b082a0
mukhammadkodir-alijonov <alijonov0608@gmail.com>
2024-11-01 03:10:40 -0700

    leetcode

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,17 +1,11 @@
-from typing import List
-
 class Solution:
-<<<<<<< HEAD
-    def hIndex(self, citations: List[int]) -> int:
-        pass
-=======
-    def canJump(self, nums: List[int]) -> bool:
-        farthest = 0
-        for i in range(len(nums)):
-            if i > farthest:
-                return False
-            farthest = max(farthest, i + nums[i])
-        return True
+    def makeFancyString(self, s: str) -> str:
+        result = []  
+        for char in s:
+            if len(result) < 2 or not (result[-1] == result[-2] == char):
+                result.append(char)
+        return ''.join(result)
 
-print(Solution().canJump([2,1,1,0,4])) # True
->>>>>>> 11ff87fc4f809355910c3ecfbc1fe9e665e75688
+solution = Solution()
+print(solution.makeFancyString("leeetcode"))  # Output: "leetcode"
+print(solution.makeFancyString("aaabaaaa"))   # Output: "aabaa"
