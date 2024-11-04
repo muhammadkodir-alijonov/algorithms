8da4c6a251858f0564190724ccb105c1b1669823
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2024-11-04 18:47:16 +0500

    Update leetcode.py

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,16 +1,8 @@
-class Solution:
-    def balancedStringSplit(self, s: str) -> int:
-        x,y=0
-        count = 0
-        n = len(s)
-        for i in range(1,n):
-            if i == 'R':
-                x+=1
-                if x == y:
-                    count += 1
-            else:
-                y+=1
-                if x == y:
-                    count +=1 
-        return count
-                
+def sortSentence(s):
+    words = s.split(" ")
+    sorted_words = sorted(words, key=lambda word: int(word[-1]))
+    result = [word[:-1] for word in sorted_words]
+    return " ".join(result)
+
+s = "is2 sentence4 This1 a3"
+print(sortSentence(s))
