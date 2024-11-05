67da86ef6f86fd26fabc7b23854eefeab46f495b
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2024-11-05 15:43:42 +0500

    Update leetcode.py

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -11,3 +11,17 @@ class Solution:
         for i in string:
             new_str += str(ord(i)-97)
         return new_str
+
+
+
+from typing import List
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h_index = 0
+        for i,citation in enumerate(citations):
+            if citation > i+1:
+                h_index = i+1
+            else:
+                break
+        return h_index
