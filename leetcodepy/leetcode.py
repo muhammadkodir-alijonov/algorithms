7530cfb26aa71318519b20a42d539dfc28da7c2e
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-11-27 21:24:40 +0500

    decodeString

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,24 +1,22 @@
-from inspect import stack
-from itertools import count
-
-
 class Solution:
-    def decodeString(self, s: str) -> str:
-        stack = []  # Stack to store previous substrings and counts
-        current_str = ""  # Current decoded string
-        current_num = 0  # To handle the number before `[`
+    def decodeString(s: str) -> str:
+        stack = []  
+        current_str = ""  
+        current_num = 0 
 
         for char in s:
-            if char.isdigit():  # If the character is a digit
-                current_num = current_num * 10 + int(char)  # Build the number
-            elif char == '[':  # If `[` is encountered, push current values onto stack
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            elif char == '[':
                 stack.append((current_str, current_num))
-                current_str = ""  # Reset the current string
-                current_num = 0  # Reset the number
-            elif char == ']':  # If `]` is encountered, pop from stack and repeat the string
+                current_str = ""  
+                current_num = 0 
+            elif char == ']': 
                 last_str, num = stack.pop()
-                current_str = last_str + current_str * num  # Repeat the current string `num` times
-            else:  # If the character is a letter, add it to the current string
+                current_str = last_str + current_str * num  
+            else: 
                 current_str += char
 
-        return current_str  # Return the final decoded string
+        return current_str
+
+print(Solution.decodeString("3[a]2[bc]"))
