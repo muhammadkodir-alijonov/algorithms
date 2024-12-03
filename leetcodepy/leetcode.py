deffcaf0643f54cc0aed2b159bc8b561564ed70a
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-12-03 09:48:05 +0500

    addSpaces

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,26 +1,14 @@
-def min_flips(a, b, c):
-    flips = 0
-    while a > 0 or b > 0 or c > 0:
-        # Har bir sonning o‘ng tarafdagi bitini ajratib olamiz
-        bit_a = a & 1
-        bit_b = b & 1
-        bit_c = c & 1
+from typing import List
 
-        # Shartlarni tekshiramiz
-        if bit_c == 0:
-            flips += bit_a + bit_b  # Ikkalasini ham 0 qilish kerak bo‘lsa
-        elif bit_c == 1:
-            if bit_a == 0 and bit_b == 0:
-                flips += 1  # Hech bo‘lmaganda bittasini 1 qilish kerak
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        ans = []
+        space_index = 0
+        for i in range(len(s)):
+            if space_index < len(spaces) and i == spaces[space_index]:
+                ans.append(" ")
+                space_index += 1
+            ans.append(s[i])
+        return "".join(ans)
 
-        # Keyingi bitlarga o‘tamiz
-        a >>= 1
-        b >>= 1
-        c >>= 1
-
-    return flips
-
-
-# Test
-a, b, c = 2, 6, 5  # Misol: a=2(010), b=6(110), c=5(101)
-print(min_flips(a, b, c))  # Natija: 3
+print(Solution().addSpaces("LeetcodeHelpsMeLearn", [8, 13, 15]))
