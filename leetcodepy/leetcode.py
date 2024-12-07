0c0b29ea7a8a5f55a29cbb1baaf404159d280da7
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-12-07 21:42:43 +0500

    minimumSize

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,14 +1,24 @@
 from typing import List
 
-class Solution:
-    def addSpaces(self, s: str, spaces: List[int]) -> str:
-        ans = []
-        space_index = 0
-        for i in range(len(s)):
-            if space_index < len(spaces) and i == spaces[space_index]:
-                ans.append(" ")
-                space_index += 1
-            ans.append(s[i])
-        return "".join(ans)
 
-print(Solution().addSpaces("LeetcodeHelpsMeLearn", [8, 13, 15]))
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        left, right = 1, max(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+            if self.count_operations(nums, mid) <= maxOperations:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
+    def count_operations(self, nums: List[int], max_size: int) -> int:
+        operations = 0
+        for num in nums:
+            if num > max_size:
+                operations += (num - 1) // max_size
+        return operations
+
+print(Solution().minimumSize([9], 2))  # 5
