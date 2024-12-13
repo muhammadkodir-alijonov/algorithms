1a7c4fefa8d00b4991a7eadd1c71ccdf746824af
muxammadqodir.alijonov <muxammadqodir.alijonov@fido.uz>
2024-12-13 19:41:41 +0500

    findScore

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,24 +1,21 @@
 from typing import List
-
-
 class Solution:
-    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        left, right = 1, max(nums)
+    def findScore(self, nums: List[int]) -> int:
+        # faqat + numlar ekan
+        # score = 0 dan boshlanadi ekan
+        # mark qilinmagan smallest num ni qidirish kerak
+        # add score endi u mavjud bo'lsa uni qo'shnilarni mark qilib qo'yamiz
+        # repeat qilamiz hammasi mark bo'maguncha
+        score = 0
+        n = len(nums)
+        visited = [False for _ in range(n)]
+        for i in range(1,n):
+            k = n
+            min_ = min(nums)
+            if not visited[i]:
+                visited[i] = True
+                score += min_
+                if n<0 and not visited[i+1] and not visited[i-1]:
+                    visited[i-1],visited[i+1] = True, True
 
-        while left < right:
-            mid = (left + right) // 2
-            if self.count_operations(nums, mid) <= maxOperations:
-                right = mid
-            else:
-                left = mid + 1
-
-        return left
-
-    def count_operations(self, nums: List[int], max_size: int) -> int:
-        operations = 0
-        for num in nums:
-            if num > max_size:
-                operations += (num - 1) // max_size
-        return operations
-
-print(Solution().minimumSize([9], 2))  # 5
+        return score
