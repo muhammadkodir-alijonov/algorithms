62ffbc23061a7f8dc562a4293a396a16d6e55584
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2024-12-27 18:18:01 +0500

    bfs

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,21 +1,29 @@
-from typing import List
-class Solution:
-    def findScore(self, nums: List[int]) -> int:
-        # faqat + numlar ekan
-        # score = 0 dan boshlanadi ekan
-        # mark qilinmagan smallest num ni qidirish kerak
-        # add score endi u mavjud bo'lsa uni qo'shnilarni mark qilib qo'yamiz
-        # repeat qilamiz hammasi mark bo'maguncha
-        score = 0
-        n = len(nums)
-        visited = [False for _ in range(n)]
-        for i in range(1,n):
-            k = n
-            min_ = min(nums)
-            if not visited[i]:
-                visited[i] = True
-                score += min_
-                if n<0 and not visited[i+1] and not visited[i-1]:
-                    visited[i-1],visited[i+1] = True, True
 
-        return score
+# Definition for a Node.
+from typing import Optional, List
+from collections import deque
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        queue = deque()
+        if root:
+            queue.append(root)
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i < size - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
