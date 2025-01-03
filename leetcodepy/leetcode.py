c4333be91388a13d5cd4b381c816515c2dd89fbe
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2025-01-03 11:00:44 +0500

    rightSideView

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,29 +1,26 @@
-
-# Definition for a Node.
+# Definition for a binary tree node.
 from typing import Optional, List
 from collections import deque
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        self.next = next
-
-
-
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        queue = deque()
-        if root:
-            queue.append(root)
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        queue = deque([root])
+        if root is None:
+            return []
         while queue:
-            size = len(queue)
-            for i in range(size):
+            n = len(queue)
+            for i in range(0, n-1):
                 node = queue.popleft()
-                if i < size - 1:
-                    node.next = queue[0]
                 if node.left:
                     queue.append(node.left)
-                if node.right:
+                elif node.right:
                     queue.append(node.right)
-        return root
+                elif i == n-1:
+                    ans.append(node.val)
+        return ans
