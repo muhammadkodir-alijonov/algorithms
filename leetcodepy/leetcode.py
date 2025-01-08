f452dbfa4694ed2171d12d549054d3870c373509
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2025-01-08 11:25:11 +0500

    minOperations

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,26 +1,31 @@
-# Definition for a binary tree node.
-from typing import Optional, List
-from collections import deque
+from typing import List
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    def minOperations(self, boxes: str) -> List[int]:
+        n_boxes = list(boxes)
         ans = []
-        queue = deque([root])
-        if root is None:
-            return []
-        while queue:
-            n = len(queue)
-            for i in range(0, n-1):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                elif node.right:
-                    queue.append(node.right)
-                elif i == n-1:
-                    ans.append(node.val)
+        for i, b in enumerate(n_boxes):
+            fw = self.forward(n_boxes, i)
+            bw = self.backward(n_boxes, i)
+            ans.append(fw + bw)
         return ans
+
+    def forward(self, n_boxes, i):
+        summ = 0
+        for b in range(i + 1, len(n_boxes)):
+            if n_boxes[b] == '1':
+                summ += b - i
+        return summ
+
+    def backward(self, n_boxes, i):
+        summ = 0
+        for b in range(i - 1, -1, -1):
+            if n_boxes[b] == '1':
+                summ += i - b
+        return summ
+
+if __name__ == "__main__":
+    solution = Solution()
+    boxes = "001011"
+    result = solution.minOperations(boxes)
+    print(result)
