4f4ace5f55f064d410e920c2e84bb7313a4086cb
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2025-02-22 17:44:46 +0500

    successfulPairs

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -2,10 +2,19 @@ from typing import List
 
 
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
-        res = []
-        for i in range(1 << n):
-            res.append(i ^ i >> 1)
-        return res
-s = Solution()
-print(s.grayCode(1))
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()  # Sort potions for binary search
+        result = []
+
+        for spell in spells:
+            left, right = 0, len(potions)
+            while left < right:
+                mid = (left + right) // 2
+                if spell * potions[mid] >= success:
+                    right = mid
+                else:
+                    left = mid + 1
+
+            result.append(len(potions) - left)
+
+        return result
