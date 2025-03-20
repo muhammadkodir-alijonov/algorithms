addb0d942d31cb3b2b61b7e3093ef0d9c69eb8cf
muxammadqodir.a <muxammadqodir.alijonov@fido.uz>
2025-03-20 09:16:33 +0500

    findMaxLength

--- leetcodepy/leetcode.py
+++ leetcodepy/leetcode.py
@@ -1,10 +1,20 @@
-import math
 from typing import List
 
 class Solution:
-    def checkPowersOfThree(self, n: int) -> bool:
-        while n > 0:
-            if n % 3 == 2:
-                return False
-            n //= 3
-        return True
+    def findMaxLength(self, nums: List[int]) -> int:
+        count = 0
+        max_len = 0
+        count_dict = {0: -1}
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count -= 1
+            else:
+                count += 1
+            if count in count_dict:
+                max_len = max(max_len, i - count_dict[count])
+            else:
+                count_dict[count] = i
+        return max_len
+
+s = Solution()
+print(s.findMaxLength([0,1,1,1,1,1,0,0,0]))
